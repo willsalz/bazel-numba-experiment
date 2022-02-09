08307a38301306de35b469ab7a797a143b596d00
Will Salisbury <willsalz@users.noreply.github.com>
2022-02-08 17:24:09 -0800

    rename main, add scikit-learn

---
+++ main.py
@@ -0,0 +1,13 @@
+import numba
+import pandas
+import numpy
+import matplotlib
+import sklearn
+from sklearn.metrics import confusion_matrix
+
+if __name__ == "__main__":
+    print(f"numba=={numba.__version__}")
+    print(f"pandas=={pandas.__version__}")
+    print(f"numpy=={numpy.__version__}")
+    print(f"matplotlib=={matplotlib.__version__}")
+    print(f"sklearn=={sklearn.__version__}")
