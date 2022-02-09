24e1a179bfc6c73e0ffab06e1cdc5ccabbde963a
Will Salisbury <willsalz@users.noreply.github.com>
2022-02-08 17:07:02 -0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+import numba
+import pandas
+import numpy
+import matplotlib
+
+if __name__ == "__main__":
+    print(numba.__version__)
+    print(pandas.__version__)
+    print(numpy.__version__)
+    print(matplotlib.__version__)
+    print("hello, bazel!")
